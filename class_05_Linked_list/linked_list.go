bf118dbcfd357e1d48c3bd053d2d75f49a6e3022
Victor Rosa <vicrosa25@gmail.com>
2025-01-18 20:53:21 +0100

    Serialize and Deserialize

--- class_05_Linked_list/linked_list.go
+++ class_05_Linked_list/linked_list.go
@@ -1,34 +1,74 @@
 package class_05_Linked_list
 
 import (
-	"errors"
-	"fmt"
+	"strconv"
+	"strings"
 )
 
-type Node[T any] struct {
-	value T
-	next  *Node[T]
+const separator = "->"
+
+type Node struct {
+	Value int
+	Next  *Node
 }
 
-type LinkedList[T any] struct {
-	head *Node[T]
-	tail *Node[T]
+type LinkedList struct {
+	head *Node
+	tail *Node
 	size int
 }
 
-func (ll *LinkedList[T]) Display() error {
-	if ll.head == nil {
-		return errors.New("display: List is empty")
+func Serialize(node *Node) string {
+	if node == nil {
+		return ""
 	}
-	current := ll.head
-	for current != nil {
-		fmt.Printf("%v->", current.value)
-		current = current.next
+	output := strconv.Itoa(node.Value) + separator
+	for node.Next != nil {
+		node = node.Next
+		output += strconv.Itoa(node.Value) + separator
 	}
-	fmt.Println()
-	return nil
+	return strings.TrimSuffix(output, separator)
 }
 
-func (ll *LinkedList[T]) Length() int {
+func Deserialize(stringRepresentation string) *Node {
+	if stringRepresentation == "" {
+		return nil
+	}
+	broken := strings.Split(stringRepresentation, separator)
+	var curr, last *Node
+	for i := len(broken) - 1; i >= 0; i-- {
+		last = curr
+		curr = &Node{
+			Value: atoi(broken[i]),
+			Next:  nil,
+		}
+		if last != nil {
+			curr.Next = last
+		}
+	}
+	return curr
+}
+
+func atoi(number string) int {
+	i, err := strconv.Atoi(number)
+	if err != nil {
+		return -1
+	}
+	return i
+}
+
+func (ll *LinkedList) Length() int {
 	return ll.size
+}
+
+func (ll *LinkedList) Prepend(value int) {
+	node := &Node{Value: value}
+	if ll.head == nil {
+		ll.head = node
+		ll.tail = node
+	} else {
+		node.Next = ll.head
+		ll.head = node
+	}
+	ll.size++
 }
--- class_05_Linked_list/linked_list_test.go
+++ class_05_Linked_list/linked_list_test.go
@@ -1,1 +1,34 @@
 package class_05_Linked_list
+
+import "testing"
+
+/*
+TestSerializeAndDeserializeLinkedList tests solution(s) with the following signature and problem description:
+
+	type Node struct {
+		Value int
+		Next *Node
+	}
+
+	func Serialize(node *Node) string
+	func Deserialize(stringRepresentation string) *Node
+
+Write a function that turns a linked list into a string representation (Serialize), and then a function
+that turns that string representation to an actual linked list (Deserialize).
+*/
+
+func TestSerializeAndDeserialize(t *testing.T) {
+	tests := []string{
+		"",
+		"1",
+		"1->2",
+		"1->2->3",
+		"1->2->3->4->5",
+	}
+	for i, test := range tests {
+		got := Serialize(Deserialize(test))
+		if got != test {
+			t.Fatalf("Failed test case %d: want %#v, got %#v", i, test, got)
+		}
+	}
+}
